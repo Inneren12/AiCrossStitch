5ca9d0844f7cbd1c4bf3274f7c5d969e73b42de7
Inneren12 <chulkov.oleksii.can@gmail.com>
2025-10-17 13:21:08 -0400

    Исправил баг и добавил новую функцию

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" apply false
 }
 
 allprojects {
